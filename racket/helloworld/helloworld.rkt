2f3d07e3bb120970c6c4b2ccdb959cd869d60175
Maciej Barć <xgqt@riseup.net>
2021-03-18 20:06:11 +0100

    racket/helloworld/helloworld.rkt: add new
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ racket/helloworld/helloworld.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+
+(require racket/gui)
+
+
+(define frame (new frame% [label "Hello World"]))
+
+(new button%
+     [parent frame]
+     [label "Hello World!"]
+     [callback
+      (lambda (button event)
+        (exit))
+      ]
+     )
+
+
+(send frame show #t)
