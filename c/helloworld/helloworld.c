98ecb9b7881539f6c7e1126181be1a55ff0f2eee
Maciej Barć <xgqt@riseup.net>
2021-04-07 19:28:24 +0200

    c/helloworld/helloworld.c: set size
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- c/helloworld/helloworld.c
+++ c/helloworld/helloworld.c
@@ -1,3 +1,12 @@
+/*
+ * requires the gtk library:
+ *   https://gitlab.gnome.org/GNOME/gtk
+ *
+ * run with:
+ *   mkdir -p ./build && cd ./build && cmake .. && make && ./helloworld
+ */
+
+
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
@@ -12,6 +21,7 @@ static void on_app_activate ( GApplication * app, gpointer data )
     GtkWidget * window = gtk_application_window_new(GTK_APPLICATION(app));
     GtkWidget * btn = gtk_button_new_with_label("Hello World!");
 
+    gtk_window_set_default_size( (GtkWindow *) window, 300, 300);
     g_signal_connect(btn, "clicked", G_CALLBACK(on_button_clicked), NULL);
     gtk_container_add(GTK_CONTAINER(window), btn);
     gtk_widget_show_all(GTK_WIDGET(window));
