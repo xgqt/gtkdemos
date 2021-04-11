7861c3a001c53fa2d2339882e39cf458685d1ef6
Maciej Barć <xgqt@riseup.net>
2021-04-11 20:28:50 +0200

    racket/helloworld: proper size; add instructions to compile; add gitignore
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- racket/helloworld/helloworld.rkt
+++ racket/helloworld/helloworld.rkt
@@ -7,17 +7,28 @@
 ;; run with:
 ;;   racket helloworld.rkt
 
+;; or compile & run with:
+;;   raco exe -o helloworld helloworld.rkt && ./helloworld
+
 
 #lang racket/base
 
 (require racket/gui)
 
 
-(define frame (new frame% [label "Hello World"]))
+(define frame
+  (new frame%
+       [label "Hello World"]
+       [height 300]
+       [width  300]
+       )
+  )
 
 (new button%
      [parent frame]
      [label "Hello World!"]
+     [stretchable-height #t]
+     [stretchable-width  #t]
      [callback
       (lambda (button event)
         (exit))
