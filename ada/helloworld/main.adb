8be5b09b8296c114e66c2bac9129a007660e10ec
Maciej Barć <xgqt@riseup.net>
2021-04-11 19:58:22 +0200

    ada/helloworld/main.adb: close window on close button click
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- ada/helloworld/main.adb
+++ ada/helloworld/main.adb
@@ -7,29 +7,43 @@
 -- FIXME: Clicking the button doesn't close the app
 
 
+with Gdk.Event;     use Gdk.Event;
 with Gtk.Button;    use Gtk.Button;
 with Gtk.Main;
+with Gtk.Widget;    use Gtk.Widget;
 with Gtk.Window;    use Gtk.Window;
 
 
 procedure Main is
 
-    Win : Gtk_Window;
-    Btn : Gtk_Button;
+   Win : Gtk_Window;
+   Btn : Gtk_Button;
+
+   function Delete_Event_Cb
+     (Self  : access Gtk_Widget_Record'Class;
+      Event : Gdk.Event.Gdk_Event)
+      return Boolean
+   is
+      pragma Unreferenced (Self, Event);
+   begin
+      Gtk.Main.Main_Quit;
+      return True;
+   end Delete_Event_Cb;
 
 begin
 
-    Gtk.Main.Init;
+   Gtk.Main.Init;
 
-    Gtk_New (Win);
-    Win.Set_Title ("Hello World");
-    Win.Set_Default_Size (300, 300);
+   Gtk_New (Win);
+   Win.Set_Title ("Hello World");
+   Win.Set_Default_Size (300, 300);
 
-    Gtk_New (Btn, "Hello World!");
-    Win.Add (Btn);
+   Gtk_New (Btn, "Hello World!");
+   Win.Add (Btn);
 
-    Win.Show_All;
+   Win.On_Delete_Event (Delete_Event_Cb'Unrestricted_Access);
+   Win.Show_All;
 
-    Gtk.Main.Main;
+   Gtk.Main.Main;
 
 end Main;
