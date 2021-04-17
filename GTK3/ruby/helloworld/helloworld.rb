1ef1dd2a097dceb794a8c2f69c0d1def5d2c0849
Maciej Barć <xgqt@riseup.net>
2021-04-17 02:31:51 +0200

    GTK3/ruby/helloworld/helloworld.rb: fix QA
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- GTK3/ruby/helloworld/helloworld.rb
+++ GTK3/ruby/helloworld/helloworld.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
 
 # requires the gtk3 library:
 #   https://github.com/ruby-gnome/ruby-gnome
@@ -7,27 +8,18 @@
 # run with:
 #   ruby helloworld.rb
 
+require 'gtk3'
 
-require "gtk3"
-
-
-window = Gtk::Window.new("Hello World")
-
+window = Gtk::Window.new('Hello World')
 window.set_size_request(300, 300)
 
-
-button = Gtk::Button.new(:label => "Hello World!")
-
-button.signal_connect "clicked" do |_widget|
+button = Gtk::Button.new(label: 'Hello World!')
+button.signal_connect 'clicked' do |_widget|
   Gtk.main_quit
 end
-
 window.add(button)
 
-
-window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
-
+window.signal_connect('delete-event') { |_widget| Gtk.main_quit }
 window.show_all
-
 
 Gtk.main
